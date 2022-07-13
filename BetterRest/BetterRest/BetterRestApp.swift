0b2c40656b5f0e0da49c1dac8e7aa79ad8147ded
Conor Nolan <IAmConorNolan@gmail.com>
2022-07-13 14:20:30 +0100

    Start Project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Conor Nolan on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
